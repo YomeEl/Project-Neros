c902195fca31c1b4aa43c3523e3a5aec69ce7591
Ярослав <umpepatopChannel@gmail.com>
2021-01-03 20:46:27 +0500

    Made Scene class more abstract

--- Engine/Scene.cs
+++ Engine/Scene.cs
@@ -6,6 +6,8 @@ namespace Project_Neros.Engine
 {
     abstract class Scene
     {
+        public bool IsActive { get; private set; } = true;
+
         protected List<IActor> actors;
         protected Camera camera;
         protected readonly RenderWindow win;
@@ -15,35 +17,20 @@ namespace Project_Neros.Engine
             this.win = win;
             Reset();
             win.Resized += WinResized;
-            win.MouseButtonPressed += OnClick;
         }
 
-        public void OnClick(object sender, SFML.Window.MouseButtonEventArgs e)
+        public void Activate()
         {
-            foreach (IActor actor in actors)
-            {
-                var actorBounds = actor.GetMapBounds();
-                if (actorBounds.Contains(e.X, e.Y))
-                {
-                    switch(actor.OnClick()[0].type)
-                    {
-                        case CommandType.Quit:
-                            win.Close();
-                            break;
-                    }
-                }
-            }
+            IsActive = true;
+
+            win.MouseButtonPressed += OnClick;
+            win.MouseMoved += OnMouseMove;
         }
-        public void OnMouseMove(object sender, SFML.Window.MouseMoveEventArgs e)
-        {
-            foreach (IActor actor in actors)
-            {
-                var actorBounds = actor.GetMapBounds();
-                actor.Selected = actorBounds.Contains(e.X, e.Y);
-            }
-        }
+
         public void AddActor(IActor actor) => actors.Add(actor);
+
         public void RemoveActor(IActor actor) => actors.Remove(actor);
+
         public virtual void Draw()
         {
             var viewRect = camera.GetBorders();
@@ -58,11 +45,24 @@ namespace Project_Neros.Engine
 
         protected abstract void InitializeElements();
 
+        protected abstract void OnClick(object sender, SFML.Window.MouseButtonEventArgs e);
+
+        protected abstract void OnMouseMove(object sender, SFML.Window.MouseMoveEventArgs e);
+
+        protected void Deactivate()
+        {
+            IsActive = false;
+
+            win.MouseButtonPressed -= OnClick;
+            win.MouseMoved -= OnMouseMove;
+        }
+
         private void Reset()
         {
             camera = new Camera(new SFML.System.Vector2f(win.Size.X, win.Size.Y));
             actors = new List<IActor>();
         }
+
         private void WinResized(object sender, SFML.Window.SizeEventArgs e)
         {
             win.SetView(new View(new FloatRect(0, 0, e.Width, e.Height)));
