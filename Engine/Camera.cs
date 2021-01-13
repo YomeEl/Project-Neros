ba6e0d6168542e8fdf9a3ad5113887e640f21c99
Ярослав <umpepatopChannel@gmail.com>
2021-01-13 11:10:19 +0500

    Changed private variables to public properties

--- Engine/Camera.cs
+++ Engine/Camera.cs
@@ -5,40 +5,35 @@ namespace Project_Neros.Engine
 {
     class Camera
     {
-        private float scale = 1f;
-        private Vector2f target;
-        private Vector2f winSize;
+        public Vector2f Target { get; set; }
+        public Vector2f WinSize { get; set; }
+        public float Scale { get; set; }
 
         public Camera(Vector2f winSize, Vector2f? target = null)
         {
-            this.winSize = winSize;
-            this.target = target.GetValueOrDefault();
+            WinSize = winSize;
+            Target = target.GetValueOrDefault();
         }
 
         public void Move(Vector2f shift)
         {
-            target += shift;
+            Target += shift;
         }
 
         public void SetWinSize(Vector2f size)
         {
-            winSize = size;
+            WinSize = size;
         }
 
         public FloatRect GetBorders()
         {
-            var topleft = (target - winSize / 2) * scale;
-            return new FloatRect(topleft, winSize * scale);
+            var topleft = (Target - WinSize / 2) * Scale;
+            return new FloatRect(topleft, WinSize * Scale);
         }
 
         public Vector2f GetRelativePosition(Vector2f absPos)
         {
-            return (absPos - target + winSize / 2) * scale;
-        }
-
-        public void SetTarget(Vector2f position)
-        {
-            target = position;
+            return (absPos - Target + WinSize / 2) * Scale;
         }
     }
 }
